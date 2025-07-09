282bbca7eefc3a2aa34c5cb8b610df700fef3b17
Caro715-ai <genesiscarolina2311@gmail.com>
2025-07-09 18:03:27 -0500

    Contorl de prueba

---
+++ simbana/src/main/java/com/conjunta/simbana/SimbanaApplication.java
@@ -0,0 +1,13 @@
+package com.conjunta.simbana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbanaApplication.class, args);
+	}
+
+}
